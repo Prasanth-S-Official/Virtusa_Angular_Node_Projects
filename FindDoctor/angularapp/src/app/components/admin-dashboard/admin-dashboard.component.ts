64f355d88e4ef87cc6b4529aa5d0e74de5f04cc6
neojarvis <techadmin@iamneo.ai>
2024-02-08 13:04:08 +0000

    Manual-save-commit-08/02/2024--18:34:06

--- FindDoctor/angularapp/src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ FindDoctor/angularapp/src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -7,9 +8,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDashboardComponent implements OnInit {
 
-  constructor() { }
+  
+  showDeletePopup = false;
+  selectedEmployee: Employee;
+  showLogoutPopup = false;
+  selectedItem: any = {};
+  showModal: boolean = false;
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private employeeService: EmployeeService
+  ) {}
+
+  navigateToAddEmployee() {
+    this.router.navigate(['/add-employee']);
+  }
+  logout() {
+    // Perform logout logic here
+    // For example, clear user authentication, navigate to the login page, etc.
+    console.log('Logout clicked');
+    // For demonstration purposes, let's navigate to the login page
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('role');
+    window.location.href = '/login';
   }
 
+  employees: any[] = []; // Declare the 'vacationRentals' property as an array of any type
+
+  ngOnInit(): void {
+    // when add vacationRental button is clicked, trigger this function getBooksByUserId()
+    this.getEmployeesByUserId();
+  }
+
+  navigateToEditEmployee(employeeId: { employeeId: number }) {
+    console.log('Employee Id to be edited', employeeId);
+    this.router.navigate(['/edit-employee', employeeId.employeeId]);
+  }
+
+  getEmployeesByUserId() {
+    this.employeeService.getEmployeesByUserId().subscribe(
+      (data) => {
+        console.log(data);
+        this.employees = data;
+      },
+      (error) => {
+        console.error('Error retrieving employees', error);
+      }
+    );
+  }
+
+  deleteEmployee(employeeId: string) {
+    this.employeeService.deleteEmployee(employeeId).subscribe(
+      (response) => {
+        console.log('Employee deleted successfully', response);
+        this.getEmployeesByUserId();
+      },
+      (error) => {
+        console.error('Error deleting employee', error);
+      }
+    );
+  }
+
+  viewInfo(event: any) {
+    console.log(event);
+    this.selectedItem = event;
+    this.toggleModal();
+  }
+
+  toggleModal() {
+    this.showModal = !this.showModal;
+  }
+
+  searchText: string = ''; // Declare the 'searchText' property as a string
+  sortValue: string = ''; // Declare the 'sortValue' property as a string
+  searchEmployeesByUserId() {
+    this.employeeService
+      .searchEmployeesByUserId(this.searchText)
+      .subscribe((event) => {
+        this.employees = event;
+      });
+  }
 }
--- FindDoctor/angularapp/src/app/services/doctor.service.ts
+++ FindDoctor/angularapp/src/app/services/doctor.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { apiUrl } from 'src/api.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DoctorService {
+  public apiUrl = apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  addDoctor(doctorData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/api/doctor`, doctorData);
+  }
+
+  getDoctorsByUserId(): Observable<any> {
+    const userId = localStorage.getItem('userId');
+    return this.http.get<any>(`${this.apiUrl}/api/doctor/user/${userId}`);
+  }
+
+  updateDoctor(doctorData: any): Observable<any> {
+    const id = doctorData.doctorId;
+    return this.http.put(`${this.apiUrl}/api/doctor/${id}`, doctorData);
+  }
+
+  deleteDoctor(doctorData: any): Observable<any> {
+    const id = doctorData.doctorId;
+    return this.http.delete(`${this.apiUrl}/api/doctor/${id}`);
+  }
+
+  getAllDoctors(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/api/doctor`);
+  }
+
+  getDoctorById(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/api/doctor/${id}`);
+  }
+
+  searchDoctorsByUserId(searchValue: string): Observable<any> {
+    const userId = localStorage.getItem('userId');
+    return this.http.get(`${this.apiUrl}/api/doctor/user/${userId}`, { params: {userId, searchValue }});
+  }
+
+  getAllUsers(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/api/users`);
+  }
+
+  searchDoctors(searchValue: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/api/doctor`, { params: { searchValue }});
+  }
+
+  sortDoctors(sortValue: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/api/doctor`, { params: { sortValue }});
+  }
+
 }
