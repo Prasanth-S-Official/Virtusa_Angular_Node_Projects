223b09a34063df1125c17921c0a55ebebe6629e6
neojarvis <techadmin@iamneo.ai>
2024-02-12 06:28:56 +0000

    Auto-save-commit-12/02/2024--11:58:52

--- FindDoctor/angularapp/src/app/components/add-doctor/add-doctor.component.ts
+++ FindDoctor/angularapp/src/app/components/add-doctor/add-doctor.component.ts
@@ -17,13 +17,13 @@ export class AddDoctorComponent {
 
   constructor(private fb: FormBuilder, private doctorService: DoctorService, private route:Router) {
     this.doctorForm = this.fb.group({
-      title: ['', Validators.required],
-      description: ['', Validators.required],
-      category: ['', Validators.required],
-      startDate: ['', Validators.required],
-      endDate: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      specialization: ['', Validators.required],
+      experience: ['', Validators.required],
       location: ['', Validators.required],
-      coverImage: [null, Validators.required],
+      availability: ['', Validators.required],
+      photo: [null, Validators.required],
     });
   }
 
@@ -36,12 +36,12 @@ export class AddDoctorComponent {
       //pass userid from local storage
       // this.doctorForm.value.userId=localStorage.getItem('userId');
       this.doctor = new Doctor();
-      this.doctor.firstName = this.doctorForm.get('title').value;
-      this.doctor.lastName = this.doctorForm.get('category').value;
-      this.doctor.specialization = this.doctorForm.get('description').value;
-      this.doctor.experience = this.doctorForm.get('startDate').value;
+      this.doctor.firstName = this.doctorForm.get('firstName').value;
+      this.doctor.lastName = this.doctorForm.get('lastName').value;
+      this.doctor.specialization = this.doctorForm.get('specialization').value;
+      this.doctor.experience = this.doctorForm.get('experience').value;
       this.doctor.location = this.doctorForm.get('location').value;
-      this.doctor.availability = this.doctorForm.get('endDate').value;
+      this.doctor.availability = this.doctorForm.get('availability').value;
       this.doctor.photo = this.photoImage;
       this.doctor.userId = localStorage.getItem('userId');
       this.doctorService.addDoctor(this.doctor).subscribe(
