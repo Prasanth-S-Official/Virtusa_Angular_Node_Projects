7b99f00be1a8bfc128319d2e626ba83c646e93ae
neojarvis <techadmin@iamneo.ai>
2024-02-12 07:54:51 +0000

    Auto-save-commit-12/02/2024--13:24:45

--- FindDoctor/angularapp/src/app/components/edit-doctor/edit-doctor.component.ts
+++ FindDoctor/angularapp/src/app/components/edit-doctor/edit-doctor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DoctorService } from 'src/app/services/doctor.service';
 
 @Component({
   selector: 'app-edit-doctor',
@@ -6,10 +8,93 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-doctor.component.css']
 })
 export class EditDoctorComponent implements OnInit {
+  doctor: any = {};
+  photoImage="";
+  constructor(private route: ActivatedRoute, private doctorService: DoctorService, private router: Router) { }
 
-  constructor() { }
+  availability: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-  ngOnInit(): void {
+  ngOnInit() {
+    const doctorId = this.route.snapshot.paramMap.get('id');
+    console.log('Doctor Ids to be edited', doctorId);
+    this.getDoctorById(doctorId);
   }
+
+  handleCheckboxChange(event: any, amenity: string): void {
+    if (event.target.checked) {
+      // Add amenity to the array if checked
+      this.doctor.amenities.push(amenity);
+    } else {
+      // Remove amenity from the array if unchecked
+      const index = this.doctor.amenities.indexOf(amenity);
+      if (index !== -1) {
+        this.doctor.amenities.splice(index, 1);
+      }
+    }
+  }
+
+  getDoctorById(id: string) {
+    this.doctorService.getDoctorById(id).subscribe(
+      (response) => {
+        this.doctor = response;
+        console.log(response)
+      },
+      (error) => {
+        console.error('Error retrieving mobile', error);
+      }
+    );
+  }
+
+  updateDoctor() {
+    this.doctor.photo=this.photoImage;
+    this.doctorService.updateDoctor(this.doctor).subscribe(
+      (response) => {
+        console.log('Vacation Rental updated successfully', response);
+        //navigate to seller dashboard
+        this.router.navigate(['/owner-dashboard']);
+      },
+      (error) => {
+        console.error('Error updating mobile', error);
+      }
+    );
+ }
+
+ 
+ goBack(): void {
+  // Navigate to the dashboard or any desired route
+  this.router.navigate(['/admin-dashboard']);
+}
+
+ handleFileChange(event: any): void {
+  const file = event.target.files[0];
+
+  if (file) {
+    this.convertFileToBase64(file).then(
+      (base64String) => {
+        this.photoImage=base64String
+      },
+      (error) => {
+        console.error('Error converting file to base64:', error);
+        // Handle error appropriately
+      }
+    );
+  }
+}
+
+convertFileToBase64(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      resolve(reader.result as string);
+    };
+
+    reader.onerror = (error) => {
+      reject(error);
+    };
+
+    reader.readAsDataURL(file);
+  });
+}
 
 }
