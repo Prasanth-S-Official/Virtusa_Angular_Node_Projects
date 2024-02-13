887018dcb3c4e4ac4d1235db905527812a5d376b
neojarvis <techadmin@iamneo.ai>
2024-02-13 08:06:08 +0000

    Auto-save-commit-13/02/2024--13:36:05

--- JobPosting/angularapp/src/app/components/edit-job/edit-job.component.ts
+++ JobPosting/angularapp/src/app/components/edit-job/edit-job.component.ts
@@ -1,15 +1,98 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { JobService } from 'src/app/services/job.service';
+      
 @Component({
   selector: 'app-edit-job',
   templateUrl: './edit-job.component.html',
   styleUrls: ['./edit-job.component.css']
-})
+}) 
 export class EditJobComponent implements OnInit {
+  job: any = {};
+  photoImage="";
+  constructor(private route: ActivatedRoute, private jobService: JobService, private router: Router) { }
 
-  constructor() { }
+  categories = ['House', 'Apartment', 'Villa', 'Cabin', 'Condo', 'Other'];
 
-  ngOnInit(): void {
+  ngOnInit() {
+    const jobId = this.route.snapshot.paramMap.get('id');
+    console.log('Job Id to be edited', jobId);
+    this.getJobsById(jobId);
   }
+
+getFormattedDate(dateString: string): string {
+    const date = new Date(dateString);
+    const formattedDate = date.toISOString().substring(0, 10); // "yyyy-MM-dd"
+    return formattedDate;
+  }
+  
+
+  getJobsById(id: string) {
+    this.jobService.getJobsById(id).subscribe(
+      (response) => {
+        this.job = response;
+        this.job.startDate = this.getFormattedDate(this.job.startDate);
+        this.job.endDate = this.getFormattedDate(this.job.endDate);
+        console.log(response)
+      },
+      (error) => {
+        console.error('Error retrieving job', error);
+      }
+    );
+  }
+
+  updateJob() {
+    this.job.coverImage=this.photoImage;
+    this.jobService.updateJob(this.job).subscribe(
+      (response) => {
+        console.log('Job updated successfully', response);
+        //navigate to seller dashboard
+        this.router.navigate(['/job-poster-dashboard']);
+      },
+      (error) => {
+        console.error('Error updating job', error);
+      }
+    );
+ }
+
+ 
+ goBack(): void {
+  // Navigate to the dashboard or any desired route
+  this.router.navigate(['/job-poster-dashboard']);
+}
+
+ handleFileChange(event: any): void {
+  const file = event.target.files[0];
+
+  if (file) {
+    this.convertFileToBase64(file).then(
+      (base64String) => {
+        this.photoImage=base64String
+      },
+      (error) => {
+        console.error('Error converting file to base64:', error);
+        // Handle error appropriately
+      }
+    );
+  }
+}
+
+convertFileToBase64(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      resolve(reader.result as string);
+    };
+
+    reader.onerror = (error) => {
+      reject(error);
+    };
+
+    reader.readAsDataURL(file);
+  });
+}
+
+
 
 }
