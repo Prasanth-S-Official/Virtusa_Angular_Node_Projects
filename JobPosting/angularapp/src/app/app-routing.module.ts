ec873246e83a9ffb1b36b6bbd3f6939b6644539a
neojarvis <techadmin@iamneo.ai>
2024-02-13 05:41:53 +0000

    Manual-save-commit-13/02/2024--11:11:52

--- JobPosting/angularapp/src/app/app-routing.module.ts
+++ JobPosting/angularapp/src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { SeekerDashboardComponent } from './components/seeker-dashboard/seeker-dashboard.component';
+import { PosterDashboardComponent } from './components/poster-dashboard/poster-dashboard.component';
+import { ErrorComponent } from './components/error/error.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistrationComponent },
-  { path: 'admin-dashboard', component: AdminDashboardComponent},
-  { path: 'patient-dashboard', component: PatientDashboardComponent},
-  { path: 'add-doctor', component: AddDoctorComponent},
-  { path: 'edit-doctor/:id', component: EditDoctorComponent},
+  { path: 'job-seeker-dashboard', component: SeekerDashboardComponent},
+  { path: 'job-poster-dashboard', component: PosterDashboardComponent},
+  // { path: 'add-doctor', component: AddDoctorComponent},
+  // { path: 'edit-doctor/:id', component: EditDoctorComponent},
   { path: 'error', component: ErrorComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/error' },
--- JobPosting/angularapp/src/app/app.module.ts
+++ JobPosting/angularapp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ErrorComponent } from './components/error/error.component';
@@ -24,7 +24,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
